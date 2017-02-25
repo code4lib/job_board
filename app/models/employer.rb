98cc4c98d249ff6f7d4e3947afe2de1d19c0854a
Chris Beer <chris@cbeer.info>
2017-02-25 13:20:07 -0800

    Add friendly id

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,3 +1,11 @@
 class Employer < ApplicationRecord
+  extend FriendlyId
+
   has_many :jobs
+
+  friendly_id :slug_candidates, use: [:slugged, :finders]
+  
+  def slug_candidates
+    [:name]
+  end
 end
--- db/migrate/20170225190652_create_employers.rb
+++ db/migrate/20170225190652_create_employers.rb
@@ -2,6 +2,7 @@ class CreateEmployers < ActiveRecord::Migration[5.1]
   def change
     create_table :employers do |t|
       t.string :name
+      t.string :slug, unique: true
       t.text :description
       t.text :address
 
