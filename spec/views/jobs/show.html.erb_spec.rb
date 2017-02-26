c692d967a7430f795e060edaad6b1cdd97a23524
Chris Beer <chris@cbeer.info>
2017-02-25 18:14:27 -0800

    Condense new job form

--- spec/views/jobs/show.html.erb_spec.rb
+++ spec/views/jobs/show.html.erb_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe "jobs/show", type: :view do
   before(:each) do
     @job = assign(:job, Job.create!(
       :title => "MyTitle",
-      :origin => "MyString",
       :url => "MyString",
       :description => "MyText",
       :job_type => :full_time,
@@ -18,7 +17,6 @@ RSpec.describe "jobs/show", type: :view do
   it "renders attributes in <p>" do
     render
     expect(rendered).to match(/MyTitle/)
-    expect(rendered).to match(/Origin/)
     expect(rendered).to match(/Url/)
     expect(rendered).to match(/MyText/)
     expect(rendered).to match(/false/)
