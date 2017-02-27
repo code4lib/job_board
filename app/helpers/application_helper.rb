ed0493d58f4da522dff46725d09ace510ef918d7
Chris Beer <chris@cbeer.info>
2017-02-26 17:47:54 -0800

    Add moderation count badge to the nav link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,10 @@ module ApplicationHelper
     end
   end
 
+  def moderation_text_with_badge
+    safe_join ['Moderation', ' ', content_tag(:span, Job.unpublished.count, class: 'badge badge-default job-unpublished-count')]
+  end
+
   def render_marked_up_description(content, format:)
     case format
     when '.html'
