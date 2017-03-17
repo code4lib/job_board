85e2b213d2dd2990ed03294ef23f3981bd5ac702
Chris Beer <chris@cbeer.info>
2017-03-17 10:16:46 -0700

    Better sanitize HTML responses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,9 +15,17 @@ module ApplicationHelper
   def render_marked_up_description(content, format:)
     case format
     when '.html'
-      sanitize content
+      white_list_sanitizer.sanitize(Loofah.fragment(content).tap { |x| x.scrub!(:prune) }.to_s, tags: %w(a div p strong b i em blockquote ul ol li table tr td thead tbody br)).html_safe
+    when '.md'
+      GitHub::Markup.render(format, content).html_safe
     else
-      GitHub::Markup.render(format || '.md', content).html_safe
+      auto_link(simple_format(content))
     end
   end
+
+  private
+
+    def white_list_sanitizer
+      Rails::Html::WhiteListSanitizer.new
+    end
 end
