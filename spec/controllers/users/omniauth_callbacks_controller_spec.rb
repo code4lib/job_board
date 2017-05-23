d039ddc0295ba52da665f7393db729e984f882f1
Chris Beer <chris@cbeer.info>
2017-05-23 13:29:58 -0700

    Add tests for the omniauth controller

---
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe Users::OmniauthCallbacksController do
+  let(:mock_user) { FactoryGirl.create(:user) }
+
+  before do
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+  end
+
+  describe '#authorize' do
+    it 'works' do
+      allow(User).to receive(:find_or_create_by).with(provider: 'mock', uid: 'uid').and_return(mock_user)
+
+      @request.env['omniauth.auth'] = double(provider: 'mock', uid: 'uid')
+
+      get :code4lib
+
+      expect(response).to redirect_to root_url
+      expect(controller.current_user).to eq mock_user
+    end
+  end
+end
