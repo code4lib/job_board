5d81d267e9059288dcf5f31f70f461b5855dff39
Chris Beer <cabeer@stanford.edu>
2019-08-18 10:38:33 -0700

    Add a separate field for salary information

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -75,7 +75,7 @@ class JobsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_params
-      params.require(:job).permit(:title, :url, :description, :user, :job_type, :telecommute, :employer_name, :location, :contact, :tag_list)
+      params.require(:job).permit(:title, :url, :description, :user, :job_type, :telecommute, :employer_name, :location, :contact, :tag_list, :salary)
     end
 
     def create_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2017_03_19_153845) do
+ActiveRecord::Schema.define(version: 2019_08_18_103000) do
 
   create_table "employers", force: :cascade do |t|
     t.string "name"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2017_03_19_153845) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.string "salary"
     t.index ["deleted_at"], name: "index_jobs_on_deleted_at"
     t.index ["employer_id"], name: "index_jobs_on_employer_id"
     t.index ["published_at"], name: "index_jobs_on_published_at"
